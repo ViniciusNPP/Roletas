8a03c52b2b0652588e0cb32b5275f85e598f37f1
Vincius <vnpp856@gmail.com>
2026-04-13 23:26:33 -0300

    Lógica do seletor de cor finalizada, com input rgb funcionando corretamente e borda do seletor mudando conforme a cor selecionada.

--- roleta/modules/utils.js
+++ roleta/modules/utils.js
@@ -1,5 +1,33 @@
-function gerarId(){
+function gerarId() {
     return `${Date.now() + (Math.random() * 1000).toFixed(0)}`
 }
 
-export {gerarId}
+const caracteresProibidos = [ // Lista de caracteres proibidos para nomes de itens
+    "<", ">",       // Impede tags HTML (XSS)
+    "/", "\\",      // Evita problemas com caminhos de diretório
+    "{", "}",       // Evita confusão com objetos JSON
+    "[", "]",       // Evita confusão com arrays
+    "(", ")",       // Impede chamadas de função maliciosas
+    "\"", "'", "`", // Evita quebra de strings no JS ou banco de dados
+    ";", ":",       // Evita injeção de comandos ou problemas de formatação
+    "=", "+",       // Caracteres de atribuição/operação
+    "&", "|",       // Operadores lógicos
+    "*",             // Caractere curinga
+    ",", ".", "-", "+" // Caracteres que podem causar problemas de formatação ou validação
+];
+
+function VerficarCaracterProibido(value) {
+    return caracteresProibidos.includes(value);
+}
+
+function VerficarSeNumero(value) {
+    return (value >= "0" && value <= "9") || !isNaN(value);
+}
+
+function PreencherRGB(obj_rgb, inputs_rgb){
+    inputs_rgb.forEach((input, index) => {
+        input.value = Object.values(obj_rgb)[index];
+    });
+}
+
+export { gerarId, VerficarCaracterProibido, VerficarSeNumero, PreencherRGB }
--- roleta/roleta.js
+++ roleta/roleta.js
@@ -3,23 +3,24 @@ import * as CustomItem from './modules/customização-itens.js';
 import * as Util from './modules/utils.js';
 import * as easing from './libs/easing.js';
 
+/*
+ LINK DAS BIBLIOTECAS DE FORA USADOS:
+    spin-whell by CrazyTim: https://github.com/CrazyTim/spin-wheel?tab=readme-ov-file
+    iro.js by jaames: https://github.com/jaames/iro.js?tab=readme-ov-file
+    easing-utils by AndrewRayCode: https://github.com/AndrewRayCode/easing-utils?tab=readme-ov-file
+ */
+
 //#region VARIÁVEIS GLOBAIS
 const container = document.querySelector('.roleta-container'); //Onde a roleta é criada
 const container_itens = document.querySelector('.itens-roleta'); //Container das configurações dos itens
 const botao_adicionar = document.querySelector('#adicionar-item'); //Botão para adicionar itens
-const caracteresProibidos = [ // Lista de caracteres proibidos para nomes de itens
-    "<", ">",       // Impede tags HTML (XSS)
-    "/", "\\",      // Evita problemas com caminhos de diretório
-    "{", "}",       // Evita confusão com objetos JSON
-    "[", "]",       // Evita confusão com arrays
-    "(", ")",       // Impede chamadas de função maliciosas
-    "\"", "'", "`", // Evita quebra de strings no JS ou banco de dados
-    ";", ":",       // Evita injeção de comandos ou problemas de formatação
-    "=", "+",       // Caracteres de atribuição/operação
-    "&", "|",       // Operadores lógicos
-    "*",             // Caractere curinga
-    ",", ".", "-", "+" // Caracteres que podem causar problemas de formatação ou validação
-];
+const container_seletor_cor = document.querySelector('#color-picker');
+let estado = {
+    cor_antiga: null,
+    seletor_cor: null,
+    seletor_aberto: false,
+    rgb_atualizado_manual: false
+}
 
 let seletor_aberto = false;
 let props = Roleta.createProps(
@@ -59,15 +60,17 @@ botao_adicionar.addEventListener('click', () => {
 });
 //#endregion
 
+let cor_preview;
+const container_iro_picker = document.querySelector('#container-color-picker');
 //#region CLICK
 container_itens.addEventListener('click', (e) => {
     const target = e.target;
     const container = target.closest('.item');
     if (!container) return;
 
-    // 1. Ação de Excluir Item
-    const btnExcluir = target.closest('.excluir-item');
-    if (btnExcluir) {
+    //Ação de Excluir Item
+    const btn_excluir = target.closest('.excluir-item');
+    if (btn_excluir) {
         if (props.items.length <= 2) return; // Segurança: mínimo de 2 itens
 
         const id_item = container.id.split("-")[1];
@@ -75,19 +78,72 @@ container_itens.addEventListener('click', (e) => {
         return;
     }
 
-    // 2. Ação de Abrir Seletor de Cor (Preview)
-    const corPreview = target.closest('.cor-preview');
-    if (corPreview && !seletor_aberto) {
-        var colorPicker = new iro.ColorPicker(corPreview, {
-            width: 150,
-            color: '#f00'
+    //Ação de Abrir Seletor de Cor (Preview)
+    cor_preview = target.closest('.cor-preview');
+    const inputs_rgb = document.querySelector('#container-input-rgb').querySelectorAll('.input-rgb');
+
+    if (cor_preview && !estado.seletor_aberto) {
+        e.stopImmediatePropagation();
+        container_seletor_cor.style.display = 'flex';
+
+        estado.seletor_cor = new iro.ColorPicker(container_iro_picker, {
+            width: container_seletor_cor.offsetWidth * 0.5,
+            color: cor_preview.style.backgroundColor,
+            layoutDirection: 'horizontal',
+            layout: [
+                {
+                    component: iro.ui.Box,
+                },
+                {
+                    component: iro.ui.Slider,
+                    options: {
+                        sliderType: 'hue'
+                    }
+                },
+            ]
         });
-        seletor_aberto = true;
+        Util.PreencherRGB(estado.seletor_cor.color.rgb, inputs_rgb);
+        container_seletor_cor.style.border = 'solid 4px ' + estado.seletor_cor.color.hexString;
+
+        estado.seletor_cor.on('color:change', (color) => {
+            if (!estado.rgb_atualizado_manual) {
+                Util.PreencherRGB(color.rgb, inputs_rgb);
+            }
+            container_seletor_cor.style.border = 'solid 4px ' + color.hexString;
+        });
+
+        estado.seletor_aberto = true;
+        estado.cor_antiga = cor_preview.style.backgroundColor;
+    }
+    else if (cor_preview && estado.seletor_aberto) {
+        estado.seletor_cor.color.set(cor_preview.style.backgroundColor);
+        Util.PreencherRGB(estado.seletor_cor.color.rgb, inputs_rgb);
+        container_seletor_cor.style.border = 'solid 4px ' + estado.seletor_cor.color.hexString;
+    }
+});
+//#region CLICK BOTÕES
+document.getElementById('button-cancel-color').addEventListener('click', () => {
+    if (estado.seletor_aberto) {
+        container_iro_picker.removeChild(container_iro_picker.firstChild);
+
+        estado.seletor_aberto = false;
+        container_seletor_cor.style.display = 'none';
+    }
+});
+
+document.getElementById('button-save-color').addEventListener('click', () => {
+    if (estado.seletor_aberto) {
+        cor_preview.style.backgroundColor = estado.seletor_cor.color.hexString;
+        container_iro_picker.removeChild(container_iro_picker.firstChild);
+
+        estado.seletor_aberto = false;
+        container_seletor_cor.style.display = 'none';
     }
 });
 //#endregion
+//#endregion
 
-//#region KEYDOWN
+//#region KEYDOWN ITENS
 container_itens.addEventListener('keydown', (e) => {
     const target = e.target;
     const isNome = target.classList.contains('nome-item');
@@ -103,9 +159,9 @@ container_itens.addEventListener('keydown', (e) => {
 
     // Lógica para CHANCE
     if (isChance) {
-        const teclas_invalidas = caracteresProibidos.includes(e.key);
-        const e_numero = e.key >= "0" && e.key <= "9";
-        
+        const e_numero = Util.VerficarSeNumero(e.key);
+        const teclas_invalidas = Util.VerficarCaracterProibido(e.key);
+
         // Bloqueia caracteres proibidos ou estouro de tamanho (3 dígitos)
         if (teclas_invalidas || (e_numero && target.value.length >= 3)) {
             e.preventDefault();
@@ -120,12 +176,54 @@ container_itens.addEventListener('keydown', (e) => {
     // Lógica para NOME
     if (isNome) {
         const tamanho_maximo = 20;
-        const teclas_invalidas = caracteresProibidos.includes(e.key);
+        const teclas_invalidas = Util.VerficarCaracterProibido(e.key);
         // Bloqueia se atingir limite e não for tecla de controle (e.key.length === 1)
         if (teclas_invalidas || (target.value.length >= tamanho_maximo && e.key.length === 1)) {
             e.preventDefault();
         }
     }
+});
+//#endregion
+//#region KEYDOWN SELETOR DE COR
+container_seletor_cor.querySelectorAll('.input-rgb').forEach(input => {
+    input.addEventListener('keydown', (e) => {
+        let digito = Util.VerficarSeNumero(e.key) ? e.key : '';
+        let numero_atual = input.value + digito;
+        let r, g, b;
+        let rgbString = 'rgb(255, 0, 0';
+
+        if (Util.VerficarSeNumero(e.key) && e.key !== " " && input.value.length < 3) {
+            if (parseInt(numero_atual) > 255) {
+                e.preventDefault();
+                return;
+            }
+
+            input.id === 'input-rgb-red' ? r = numero_atual : r = estado.seletor_cor.color.rgb.r;
+            input.id === 'input-rgb-green' ? g = numero_atual : g = estado.seletor_cor.color.rgb.g;
+            input.id === 'input-rgb-blue' ? b = numero_atual : b = estado.seletor_cor.color.rgb.b;
+            rgbString = `rgb(${r}, ${g}, ${b})`;
+            estado.rgb_atualizado_manual = true;
+
+            estado.seletor_cor.color.set(rgbString);
+        }
+
+        else if (e.key.length > 1) {
+
+            numero_atual = input.value.replace(/.$/, '');
+            input.id === 'input-rgb-red' ? r = numero_atual : r = estado.seletor_cor.color.rgb.r;
+            input.id === 'input-rgb-green' ? g = numero_atual : g = estado.seletor_cor.color.rgb.g;
+            input.id === 'input-rgb-blue' ? b = numero_atual : b = estado.seletor_cor.color.rgb.b;
+            rgbString = `rgb(${r}, ${g}, ${b})`;
+            estado.rgb_atualizado_manual = true;
+
+            estado.seletor_cor.color.set(rgbString);
+            return;
+        }
+
+        if (!Util.VerficarSeNumero(e.key) || Util.VerficarCaracterProibido(e.key) || e.key === " " || input.value.length >= 3) {
+            e.preventDefault();
+        }
+    });
 });
 //#endregion
 
@@ -159,7 +257,7 @@ container_itens.addEventListener('blur', (e) => {
             target.value = nome_antigo;
             return;
         }
-        
+
         if (target.value === nome_antigo) return;
 
         CustomItem.alterarNome(roleta, props, target.value, id_item);
